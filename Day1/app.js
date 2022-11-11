c2306776ac0929bf146864820e17dcf98c3d3904
Voux-Nguyen <nguyenvunguyen720@gmail.com>
2022-11-11 21:32:12 +0700

    abdfryo

---
+++ Day1/app.js
@@ -0,0 +1,32 @@
+// let array = 
+
+
+
+
+
+
+// function sayGoodbye() {
+//     console.log("Nguyên Vũ")
+// }
+// sayGoodbye();
+
+
+// function doubleKill(a, b){
+//     console.log(a + "" b + "");
+// }
+// doubleKill("Nguyên", "Vũ");
+
+
+// function sayHello(){
+//      return "Nguyên Vũ";
+// }
+// console.log(sayHello());
+
+
+
+
+function trapezoid(dayLon, dayBe, chieuCao){
+    return  ( dayLon + dayBe ) / chieuCao 
+}
+console.log(trapezoid(10, 5, 2));
+
